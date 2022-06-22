e145a7604dc5e9eda066684562a8d36de6a32b4a
pilarochoa <marleny.ochoa@est.iudigital.edu.co>
2022-06-22 16:14:44 -0500

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import { useState , useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavbarComponent from './components/NavbarComponent';
+import Natura from './components/Natura';
 
 function App() {
+  const [nombres, setNombres] = useState([]);
+  
+  
+  useEffect(() => {
+    const servicio = () => {
+      /* fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => {
+        console.log('respuesta del servicio consumido = ', json);
+        setNombres(json);
+      }) */
+    }
+   
+    servicio();
+    setNombres(['Edison', 'Marleny', 'Gloria', 'Eliza', 'Luciana ']);
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <NavbarComponent />
+      <Natura />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {nombres.map((nom) => {
+          return(
+            <p>
+              Edit <code>src/App.js</code> {nom}.
+            </p>
+            
+          )
+        })}
+      
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +45,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/Natura.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap';
+import '../styles/Natura/natura.css';
+
+// function Natura() {
+const Natura = () => {
+    return (
+        <div>
+          <h3>Nuestros Productos</h3>
+          <p>Los productos que ofrecemos sobresalen por sus buenas prácticas de producción, poseen 
+            ingredientes naturales<br></br> y no son testeados en animales, promoviendo la conservación, el 
+            cuidado del medio ambiente y el no maltatrato a los animales.</p>
+          <Carousel className="carousel-natura" variant="dark">
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://drive.google.com/uc?export=view&id=1Bazz6ml_kE04LguLKukR8nJLFrNLoyAC"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h5>Frescor Acaí</h5>
+                <p>Aroma exclusivo del la fruta del Acaí.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://drive.google.com/uc?export=view&id=1mVRoU8Pbc6KzRzhdobXjVU6PrGOzQRjS"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h5>Aguas de Cedro</h5>
+                <p>Disfruta del exquisito aroma del Cedro</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://drive.google.com/uc?export=view&id=1EYBb1t_t3teK0OkFcsZUoXUwNUV-EOfb"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h5>Kaiak Urbe</h5>
+                <p>Fragancia Masculina, Exclusiva Natura</p>
+              
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+          <p>Buscamos ofrecer productos de calidad que beneficien a nuestros clientes y
+            que sean amigables con el medio ambiente </p>
+           
+           <h3>Natura</h3>
+            <video> 
+            <source src="https://drive.google.com/uc?export=view&id=1caLq6p45EFxTuiuJVnLLzBQ82JCmsIvu" type='video/mp4'/>
+              </video>
+           
+        </div>
+    );
+}
+
+export default Natura;
